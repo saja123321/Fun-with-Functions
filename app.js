cefacc337739ead6ed81d78dd15846cd36ed2a93
Saja Malkawi <sajamalkawi95@gmail.com>
2021-07-29 19:48:31 +0300

    resolve problem3 using sum and multibly function

--- app.js
+++ app.js
@@ -71,11 +71,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let s = 0;
+  for (let i = 0; i < 3; i++) {
+    let arrSum = sum(sumArr[i], s);
+    s = arrSum[0];
+  }
+  return [s, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${s} is their sum.`];
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
