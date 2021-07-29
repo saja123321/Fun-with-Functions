e6087bd7c897ccf4e1919ddc7d2b6e3d244a65b8
Saja Malkawi <sajamalkawi95@gmail.com>
2021-07-29 16:59:40 +0300

    solve problem2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   let total = a + b;
-  return [total,  `The sum of ${a} and ${b} is ${total}.`];
+  return [total, `The sum of ${a} and ${b} is ${total}.`];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -24,11 +24,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let c =a * b ;
+  return [c, `The product of ${a} and ${b} is ${c}.` ];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -85,7 +86,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
